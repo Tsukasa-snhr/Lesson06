b51da23d4af721ec1aa8e42a83c23ec4241dc663
Tsukasa-snhr <tsukasasandesu@gmail.com>
2025-04-06 11:01:44 +0900

    Add files via upload

--- MyJava/Practice07_01.java
+++ MyJava/Practice07_01.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Lesson07_01 {
+public class Practice07_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("任意の数を入力してください: ");
-        double inputNum = sc.nextInt();
+        System.out.print("数を入力してください: ");
+        double inputNum = sc.nextDouble();
         System.out.println("入力された値は " + inputNum + " です。");
         sc.close();
     }
